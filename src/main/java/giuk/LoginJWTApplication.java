c27a69e5aadf170e16ac06fb6dada3fd8c183136
Giukviral <giuk1175@viralpick.co.kr>
2022-01-11 12:24:07 +0900

    git-test

--- src/main/java/giuk/LoginJWTApplication.java
+++ src/main/java/giuk/LoginJWTApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package giuk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
-public class HelloApplication {
+public class LoginJWTApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloApplication.class, args);
+		SpringApplication.run(LoginJWTApplication.class, args);
 	}
 
 }
--- src/test/java/giuk/LoginJWTApplicationTests.java
+++ src/test/java/giuk/LoginJWTApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package giuk;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HelloApplicationTests {
+class LoginJWTApplicationTests {
 
 	@Test
 	void contextLoads() {
